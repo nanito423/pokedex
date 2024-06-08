117f0827d5a06d7ac17996d2543c770b4040c761
Daniel Pérez Zamora <danielpz1155@gmail.com>
2024-06-08 15:52:35 +0100

    Modal terminado

--- src/components/ModalPokemon.jsx
+++ src/components/ModalPokemon.jsx
@@ -1,22 +1,24 @@
 import { IconX } from "@tabler/icons-react";
 import React from "react";
 import { colorByStat, colorByType } from "../constants/pokemon";
+import Evolutions from "./Evolutions";
 
 const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
   console.log(pokemon);
   return (
     <section
-      className={`fixed top-0 left-0 right-0 bg-green-400 h-screen transition-all duration-500 ${
+      className={`fixed top-0 left-0 right-0 h-screen transition-all duration-500 ${
         showModal ? "visible opacity-100" : "invisible opacity-0"
-      }`}
+      } ${colorByType[pokemon?.types[0]]}`}
     >
       <button onClick={onCloseModal} className="bg-white absolute top-4 right-4 rounded-md p-1 transition-all hover:scale-105 pixelated">
         <IconX size={34} stroke={4} />
       </button>
-      <article className={`bg-white capitalize h-[85%] absolute w-full rounded-tl-3xl rounded-tr-3xl text-center transition-all duration-500 px-4 grid gap-2 content-start ${showModal ? "bottom-0" : "-bottom-full"}`}>
-          <header>
-            <img src={pokemon?.image} alt="" />
+      <article className={`bg-white capitalize h-[85%] absolute w-full rounded-tl-3xl rounded-tr-3xl text-center transition-all duration-500 ${showModal ? "bottom-0" : "-bottom-full"}`}>
+          <header className="absolute left-1/2 -translate-x-1/2 -translate-y-[90%] scale-[180%]">
+            <img src={pokemon?.image} alt="" className="pixelated" />
           </header>
+          <div className="overflow-y-auto px-4 pt-12 grid gap-2 content-start h-full hidden-scroll">
           <span className="text-slate-400 text-sm font-semibold">Nº {pokemon?.id}</span>
           <h2 className="font-bold text-2xl">{pokemon?.name}</h2>
           <ul className="flex gap-2 justify-center">
@@ -30,17 +32,17 @@ const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
         </div>
         {/* Altura y peso */}
         <section className="grid grid-cols-2 gap-4">
-          <div>
+          <div className="grid gap-2">
             <h4 className="font-bold">Height</h4>
             <span className="bg-slate-100 block rounded-full p-1">0.7m</span>
           </div>
-          <div>
+          <div className="grid gap-2">
             <h4 className="font-bold">Weight</h4>
             <span className="bg-slate-100 block rounded-full p-1">6.9kg</span>
           </div>
         </section>
         {/* Habilidades */}
-        <section>
+        <section className="grid gap-2">
             <h4 className="font-bold">Abilities</h4>
             <ul className="grid grid-cols-2 gap-4">
               {
@@ -52,11 +54,11 @@ const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
             </ul>
         </section>
         {/* Stats */}
-        <section>
+        <section className="grid gap-2">
           <h4 className="font-bold">Stats</h4>
           <ul className="flex justify-center gap-3 flex-wrap">
             {pokemon?.stats.map((stat) => (
-              <li className={`bg-slate-100 p-1 rounded-full ${colorByStat[stat.name]}`} key={stat.name}>
+              <li className={` p-1 rounded-full ${colorByStat[stat.name]}`} key={stat.name}>
                 <div className="rounded-full w-[28px] aspect-square grid place-content-center">
                   <span className="text-xs text-white font-semibold">{stat.name}</span>
                 </div>
@@ -65,9 +67,11 @@ const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
             ))}
           </ul>
         </section>
-        <section>
+        <section className="grid gap-2">
           <h4 className="font-bold">Evolutions</h4>
+          <Evolutions evolutions={pokemon?.evolutions ?? []} />
         </section>
+          </div>
       </article>
     </section>
   );
